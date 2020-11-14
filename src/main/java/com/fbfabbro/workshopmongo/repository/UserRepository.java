c3f76ca5800b46241bb896c092e0d93f33f89975
fbfabbro <fbfabbro@gmail.com>
2020-11-14 07:35:36 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/fbfabbro/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.fbfabbro.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fbfabbro.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/fbfabbro/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.fbfabbro.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fbfabbro.workshopmongo.domain.User;
+import com.fbfabbro.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
