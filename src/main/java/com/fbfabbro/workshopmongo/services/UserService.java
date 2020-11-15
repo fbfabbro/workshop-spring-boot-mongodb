41e89885d9345da8c705c524cce33272804d7932
fbfabbro <fbfabbro@gmail.com>
2020-11-15 07:06:30 -0300

    Obtendo um usuário por id

--- src/main/java/com/fbfabbro/workshopmongo/services/UserService.java
+++ src/main/java/com/fbfabbro/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.fbfabbro.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fbfabbro.workshopmongo.domain.User;
 import com.fbfabbro.workshopmongo.repository.UserRepository;
+import com.fbfabbro.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
